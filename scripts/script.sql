0ff5aabda6d6f1d421b38d094bc4e57fa7128b0a
Emiliano Giusto <emigiusto@gmail.com>
2019-11-10 11:08:22 -0300

    inicializandoproyecto

---
+++ scripts/script.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE queveohoy;
+
+USE queveohoy;
+
+CREATE TABLE `pelicula` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `duracion` int(5) NOT NULL,
+  `director` varchar(400) NOT NULL,
+  `anio` int(5) NOT NULL,
+  `fecha_lanzamiento` datetime,
+  `puntuacion` int(2),
+  `poster` varchar(300),
+  `trama` varchar(700),
+  PRIMARY KEY (`id`)
+);
